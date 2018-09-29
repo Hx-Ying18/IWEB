887fe6226ac710b962dd84e3667f5e7b095161e7
Adrian Blazquez <ablazleon@ono.com>
2018-09-29 21:13:07 +0200

    Have the error, but works properly

---
+++ Cubo Flotando/CubeModel.swift
@@ -0,0 +1,54 @@
+//
+//  CubeModel.swift
+//  Cubo Flotando
+//
+//  Created by Adrian on 23/09/2018.
+//  Copyright © 2018 Adrián Blázquez León. All rights reserved.
+//
+
+import Foundation
+
+class CubeModel {
+    
+    var L = 30.0
+    private var g = 9.8
+    lazy var w = sqrt((2*g)/L) // It is found when neccesary.
+    //   private var A = 1.0
+    
+    // This methods must be public so it can be acceced to them from the viewCOntroller
+    /**
+     Give the position of the cube at time t.
+     
+     parameter t
+     
+     return pos
+     */
+    func posAtTime(_ t: Double) -> (Double) {
+        return (1/2)*L*cos(w*t)
+    }
+    
+    /**
+     Give the speed at time t
+     
+     parameter t
+     
+     return speed
+     */
+    func speedAtTime(_ t: Double) -> (Double) {
+        return -(1/2)*L*w*sin(w*t)
+    }
+    
+    /**
+     Give the speed at time t
+     
+     parameter t
+     
+     return speed
+     */
+    func accAtTime(_ t: Double) -> (Double) {
+        return -g*cos(w*t)
+    }
+    
+}
+
+
--- Cubo Flotando/ViewController.swift
+++ Cubo Flotando/ViewController.swift
@@ -1,4 +1,4 @@
-//
+
 //  ViewController.swift
 //  Cubo Flotando
 //
@@ -8,18 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// Each FunctionView of the screen has a certain name.
 
+
+
+class ViewController: UIViewController, FunctionViewDataSource {
+    
+    @IBOutlet weak var posTimeFunctionView: FunctionView!
+    
+    @IBOutlet weak var speedTimeFunctionView: FunctionView!
+    
+    @IBOutlet weak var accTimeFunctionView: FunctionView!
+    
+    @IBOutlet weak var speedPosFunctionView: FunctionView!
+    
+    
+    
+    var L: Double = 0.0 {
+        didSet{
+            posTimeFunctionView.setNeedsDisplay()
+            speedTimeFunctionView.setNeedsDisplay()
+            accTimeFunctionView.setNeedsDisplay()
+            speedPosFunctionView.setNeedsDisplay()
+        }
+    }
+    
+    @IBAction func updateLado(_ sender: UISlider) {
+        
+        
+    }
+    
+    var cubeModel = CubeModel() // It is created an object of class cubeModel
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // It is stored
+        posTimeFunctionView.dataSource = self
+        speedTimeFunctionView.dataSource = self
+        accTimeFunctionView.dataSource = self
+        speedPosFunctionView.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func startTimeOfFunctionView(_ functionView: FunctionView) -> Double {
+        return 0
+    }
+    
+    func endTimeOfFunctionView(_ functionView: FunctionView) -> Double {
+        return 10
+    }
+    
+    /**
+     Depend on the FunctionView and time ti gives the matching point.
+     */
+    func pointOfFunctionView(_ functionView: FunctionView, atTime time: Double) -> Point {
+        switch functionView {
+        case posTimeFunctionView :
+            return Point(x: time, y: cubeModel.posAtTime(time))
+        case speedTimeFunctionView :
+            return Point(x: time, y: cubeModel.speedAtTime(time))
+        case accTimeFunctionView :
+            return Point(x: time, y: cubeModel.accAtTime(time))
+        case speedPosFunctionView :
+            return Point(x: cubeModel.posAtTime(time), y: cubeModel.speedAtTime(time))
+        default:
+            return Point(x: 0,y: 0) // Whichever point
+        }
+    }
+    
 }
-
